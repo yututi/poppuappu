739e093fcbf76b4d7fe317a5bc37449674011d46
yututi <y.tsuchiya0207@gmail.com>
2019-08-17 23:36:59 +0900

    fix miss configuration

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,14 +1,16 @@
 import { PluginObject } from 'vue';
 import { Popup } from './poppuappu/popup';
-import './poppuappu/index'
+import Poppuappu from './poppuappu/index'
+
+export default Poppuappu;
 
 declare module 'vue/types/vue' {
 
     interface Vue {
-        $popup: Popup
+        $poppuappu: Popup
     }
 
     interface VueConstructor {
-        $popup: Popup
+        $poppuappu: Popup
     }
 }
